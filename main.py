0c302abf66af53f326d85914f17a7b09e6da7a2f
Matěj Brachtl <bra42176@spseol.cz>
2025-03-04 20:15:55 +0100

    Přechod na XPOS, test.html

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 import uvicorn
 import nlp # import the nlp.py file
 
@@ -8,7 +10,11 @@ app = FastAPI()
 
 app.mount("/page", StaticFiles(directory="docs", html=True), name="static")
 
-@app.get("/")
+@app.get("/test/test.html", response_class=HTMLResponse)
+def read_root():
+    return open("test.html", "r").read()
+
+@app.get("/pos/{input_sentence}")
 def read_root(input_sentence: str):
     print(input_sentence)
     return nlp.get_pos_sentence(input_sentence)
--- nlp.py
+++ nlp.py
@@ -2,31 +2,69 @@ import stanza
 import stanza.models
 import stanza.models.common
 import stanza.models.common.doc
-nlp = stanza.Pipeline("cs") # load czech language model
+nlp = stanza.Pipeline("cs", download_method=stanza.DownloadMethod.REUSE_RESOURCES) # load czech language model
 
-convertion = {
+upos_convertion = {
     "NOUN": "Podstatné jméno",
     "VERB": "Sloveso",
     "ADJ": "Přídavné jméno",
     "ADV": "Příslovce",
     "PRON": "Zájmeno",
-    # "DET": "Člen",
+    "DET": "Determinátor",
     "ADP": "Předložka",
     "NUM": "Číslovka",
     "CONJ": "Spojka",
     "PART": "Částice",
-    "INTJ": "Citoslovce"
+    "INTJ": "Citoslovce",
+    "SCONJ": "Podřadící spojka",
+    "AUX": "Pomocné sloveso",
+    "SYM": "Symbol",
+    "X": "Neznámé",
+    # "PUNCT": "Interpunkce",
+    # "SPACE": "Mezera",
+    "PROPN": "Vlastní jméno",
+    "CCONJ": "Souřadící spojka"     
+}
+
+xpos_conversion = { # hypotéza
+    "N": "Podstatné jméno",
+    "A": "Přídavné jméno",
+    "V": "Sloveso",
+    "D": "Příslovce",
+    "R": "Předložka",
+    "P": "Zájmeno",
+    "J": "Spojka",
+    "C": "Číslovka",
+    "T": "Částice"
 }
 
 # input_text = "Příliš žluťoučký kůň úpěl ďábelské ódy."
-def get_pos_sentence(input_text):
+def get_upos_sentence(input_text):
     doc = nlp(input_text)
+    print(doc.sentences)
 
     for sentence in doc.sentences:
         result = {}
         for word in sentence.words:
             try:
-                result[word.text] = convertion[word.upos]
+                result[word.text] = upos_convertion[word.upos]
             except KeyError:
                 pass
         return result
+    
+def get_xpos_sentence(input_text):
+    doc = nlp(input_text)
+    print(doc.sentences)
+# Udělal to moc rychle, a proto jej nevzali na naši školu.
+    for sentence in doc.sentences:
+        result = {}
+        for word in sentence.words:
+            try:
+                result[word.text] = xpos_conversion[word.xpos[0]]
+            except KeyError:
+                pass
+        return result
+    
+if __name__ == "__main__":
+    # print(get_upos_sentence(input("Enter a sentence: ")))
+    print(get_xpos_sentence(input("Enter a sentence: ")))
