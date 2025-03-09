48a007f562cf13d86acb7a7e8adb76e9da1c9c43
Matěj Brachtl <bra42176@spseol.cz>
2025-03-09 16:17:28 +0100

    docs Update

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 import uvicorn
-import nlp_stanza # import the nlp_stanza.py file
+# import nlp_stanza # import the nlp_stanza.py file
 
 
 app = FastAPI()
 
-app.mount("/page", StaticFiles(directory="docs", html=True), name="static")
+app.mount("/Czech-Education-App/home", StaticFiles(directory="docs/home", html=True), name="home")
+app.mount("/Czech-Education-App/test", StaticFiles(directory="docs/test", html=True), name="test")
+app.mount("/Czech-Education-App/images", StaticFiles(directory="docs/images"), name="images")
+app.mount("/Czech-Education-App/profile", StaticFiles(directory="docs/profile", html=True), name="profile")
+app.mount("/Czech-Education-App/settings", StaticFiles(directory="docs/settings", html=True), name="settings")
+app.mount("/Czech-Education-App/", StaticFiles(directory="docs", html=True), name="static")
+
+# @app.get("/page/", response_class=HTMLResponse)
+# def serve_index():
+#     return open("docs/home/index.html", "r").read()
 
 @app.get("/test/test.html", response_class=HTMLResponse)
 def read_root():
