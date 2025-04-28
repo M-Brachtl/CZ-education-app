3518a19d8f5127c54d74f9e9735fdc52546aa640
Matěj Brachtl <bra42176@spseol.cz>
2025-04-28 20:07:05 +0200

    Frontend update

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ import nlp_stanza # import the nlp_stanza.py file
 
 app = FastAPI()
 
+# složky v docs: home, friends, images, profile, kategorie, slovni-druhy, leaderboard
 app.mount("/Czech-Education-App/home", StaticFiles(directory="docs/home", html=True), name="home")
-app.mount("/Czech-Education-App/test", StaticFiles(directory="docs/test", html=True), name="test")
+app.mount("/Czech-Education-App/friends", StaticFiles(directory="docs/friends", html=True), name="friends")
 app.mount("/Czech-Education-App/images", StaticFiles(directory="docs/images"), name="images")
 app.mount("/Czech-Education-App/profile", StaticFiles(directory="docs/profile", html=True), name="profile")
-app.mount("/Czech-Education-App/settings", StaticFiles(directory="docs/settings", html=True), name="settings")
+app.mount("/Czech-Education-App/kategorie", StaticFiles(directory="docs/kategorie", html=True), name="kategorie")
+app.mount("/Czech-Education-App/slovni-druhy", StaticFiles(directory="docs/slovni-druhy", html=True), name="slovni-druhy")
+app.mount("/Czech-Education-App/leaderboard", StaticFiles(directory="docs/leaderboard", html=True), name="leaderboard")
+
 app.mount("/Czech-Education-App/", StaticFiles(directory="docs", html=True), name="static")
 
-# @app.get("/page/", response_class=HTMLResponse)
-# def serve_index():
-#     return open("docs/home/index.html", "r").read()
+# přístup k homepage: http://localhost:8000/Czech-Education-App/home
 
 @app.get("/test/test.html", response_class=HTMLResponse)
 def read_root():
@@ -33,5 +35,7 @@ def read_root(input_sentence: str):
     print(input_sentence)
     return nlp_stanza.get_morphology_sentence(input_sentence)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# spustit příkazem: uvicorn main:app (--reload: pro automatické reloadování) (--host=: pro přístup z jiného zařízení) (--port=8000: pro změnu portu)
